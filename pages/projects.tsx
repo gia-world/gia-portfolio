ac00b388e2ea46d9748cc215d77408e2d8e3403b
gia-world <gia.jihye.j@gmail.com>
2022-11-12 23:02:19 +0900

    project-items & 마무리

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,17 +1,83 @@
 import Head from "next/head";
 import React from "react";
 import Layout from "../components/layout";
+import ProjectItem from "../components/projects/project-item";
 import { TOKEN, DATABASE_ID } from "../config";
 
-const Projects = () => {
+interface Props {
+  notionData: {
+    results: Result[];
+  };
+}
+export interface Result {
+  id: string;
+  cover: {
+    file?: {
+      url: string;
+    };
+    external: {
+      url: string;
+    };
+  };
+  properties: {
+    Name: {
+      title: [
+        {
+          plain_text: string;
+        }
+      ];
+    };
+    Github: {
+      url: string;
+    };
+    Deployment: {
+      url: string;
+    };
+    Description: {
+      rich_text: [
+        {
+          plain_text: string;
+        }
+      ];
+    };
+    Tags: {
+      multi_select: [
+        {
+          id: string;
+          name: string;
+        }
+      ];
+    };
+    WorkPeriod: {
+      date: {
+        start: string;
+        end: string;
+      };
+    };
+  };
+}
+const Projects = ({ notionData }: Props) => {
+  // console.log(`(클라이언트사이드) ${projectNames}`);
   return (
     <Layout>
-      <Head>
-        <title>코딩하지아</title>
-        <meta name="description" content="코딩하지아" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>Projects</h1>
+      <div className="flex flex-col items-center justify-center min-h-screen mb-10 px-6">
+        <Head>
+          <title>코딩하지아</title>
+          <meta name="description" content="코딩하지아" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <h1 className="text-4xl font-bold sm:text-6xl">
+          총 프로젝트 :
+          <span className="pl-4 text-blue-500">
+            {notionData.results.length}
+          </span>
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 py-10 gap-8 w-full">
+          {notionData.results.map((it: any) => (
+            <ProjectItem key={it.id} data={it} />
+          ))}
+        </div>
+      </div>
     </Layout>
   );
 };
@@ -24,11 +90,19 @@ export async function getStaticProps() {
     method: "POST",
     headers: {
       accept: "application/json",
-      "Notion-Version": "2022-06-28",
+      "Notion-Version": "2022-02-22",
       "content-type": "application/json",
       Authorization: `Bearer ${TOKEN}`,
     },
-    body: JSON.stringify({ page_size: 100 }),
+    body: JSON.stringify({
+      sorts: [
+        {
+          property: "Name",
+          direction: "ascending",
+        },
+      ],
+      page_size: 100,
+    }),
   };
 
   const res = await fetch(
@@ -38,10 +112,15 @@ export async function getStaticProps() {
 
   const notionData = await res.json();
 
-  console.log(notionData);
-  // console.log(TOKEN);
+  // console.log(notionData);
+
+  const projectNames = notionData.results.map(
+    (it: Result) => it.properties.Name.title[0].plain_text
+  );
+
+  console.log(`(서버사이드)projectNames:${projectNames}`);
 
   return {
-    props: {}, // will be passed to the page component as props
+    props: { notionData }, // will be passed to the page component as props
   };
 }
