72629c9def10b0b1f23a0e4be0f7a56f7177e8de
Gia <gia.jihye.j@gmail.com>
2022-12-22 16:49:02 +0900

    캐시 삭제

--- config/index.tsx
+++
@@ -1,4 +0,0 @@
-// 설정에 대한 파일 가져오기
-// dot env npm 이용
-export const DATABASE_ID = process.env.NOTION_DATABASE_ID;
-export const TOKEN = process.env.NOTION_TOKEN;
--- types/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "animation.json";
-declare module "*.png";
-declare module "*.jpg";
-declare module "*.jpeg";
